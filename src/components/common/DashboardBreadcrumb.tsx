72e338c84131bdbed4f9d74dda9b795c5d85c4b0
a-thapamagar7 <ayushtmb7@gmail.com>
2025-11-30 17:19:09 +0545

    Initalised breadcrumb component and added to data page

---
+++ src/components/common/DashboardBreadcrumb.tsx
@@ -0,0 +1,32 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/shadcn-components/ui/breadcrumb";
+import { ChevronRight } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const DashboardBreadCrumb = () => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/">Items</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
+          <ChevronRight />
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbPage>Data</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default DashboardBreadCrumb;
